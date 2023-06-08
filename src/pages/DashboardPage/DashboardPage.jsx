9997a05147210d3be6481d6bd2b717c6c1d2ad82
Volodymyr Horbatiuk <vladimir.a.gorbatyuk@gmail.com>
2023-06-08 19:33:28 +0300

    redeploy

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,28 +1,28 @@
-import { HeroContainer, HeroBtn } from "../../components/Hero/Hero.styled";
-import { Test } from "../../components/test/Test";
+import { HeroContainer, HeroBtn } from '../../components/Hero/Hero.styled';
+import { Test } from '../../components/test/Test';
 
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function DashboardPage() {
-  const tabs = ["Test", "Statistics"];
-  const [tab, setTab] = useState("Test");
+  const tabs = ['Test', 'Statistics'];
+  const [tab, setTab] = useState('Test');
   return (
     <>
       <HeroContainer>
-        {tabs.map((item) => {
+        {tabs.map(item => {
           return (
             <HeroBtn
               type="button"
               name={item}
               key={item}
-              onClick={(e) => setTab(item)}
+              onClick={e => setTab(item)}
             >
               {item}
             </HeroBtn>
           );
         })}
-        {tab === "Test" && <Test key="testTab" />}
-        {tab === "Statistics" && <h2>UPS</h2>}
+        {tab === 'Test' && <Test key="testTab" />}
+        {tab === 'Statistics' && <h2>UPS</h2>}
       </HeroContainer>
     </>
   );
